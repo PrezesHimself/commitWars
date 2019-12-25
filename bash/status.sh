8b0a0085bc3f84af77b1f0a45b45c5a71ad7fd51
Mateusz Rorat <mateusz.rorat.ctr@sabre.com>
2019-12-25 22:28:48 +0100

    dunno whats wrong with my bash .sh on heroku

--- bash/status.sh
+++ bash/status.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 DIR="repository/.git"
-if [ -d "$DIR" ]; then
-  RESULTS=()
-  output=$(cd repository && git status)
-  RESULTS+=("$output")
+RESULTS=()
+output=$(cd repository && git status)
+RESULTS+=("$output")
 
-  echo
-  printf "%s" "${RESULTS[@]}"
-  echo
-else
-    ###  Control will jump here if $DIR does NOT exists ###
-  echo "Error: ${DIR} not found. Can not continue."
-  exit 1
-fi
+echo
+printf "%s" "${RESULTS[@]}"
+echo
