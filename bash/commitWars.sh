515bb91c69032e3eb12b59dbe7672fbd3c7839c2
Mateusz Rorat <mateusz.rorat.ctr@sabre.com>
2019-12-31 10:01:37 +0100

    fix script

--- bash/commitWars.sh
+++ bash/commitWars.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-RESULTS=()
-output=$(cd repository && git log --all --no-merges --pretty=format:"%hŶ%anŶ%adŶ%sŶ" --author="$1" --since="$2" --before="$3")
-RESULTS+=("$output")
-
-echo
-printf "%s" "${RESULTS[@]}"
-echo
+echo $(cd repository && git log --all --no-merges --pretty=format:"%hŶ%anŶ%adŶ%sŶ" --author="$1" --since="$2" --before="$3")
