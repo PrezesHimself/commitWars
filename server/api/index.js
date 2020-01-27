1ea71653929bc4113f00d0c3e0be72764028804a
Mateusz Rorat <mateusz.rorat.ctr@sabre.com>
2020-01-27 16:57:15 +0100

    fakun rally

--- server/api/index.js
+++ server/api/index.js
@@ -36,8 +36,9 @@ router.get(
                 const result = {
                     commits: []
                 };
-
                 const data = stdout.split('Ŷ');
+                console.log(data.length)
+                if(stderr) return res.send(stderr);
 
                 for (let i = 0; i < data.length; i+=4) {
                     result.commits.push({
@@ -45,9 +46,34 @@ router.get(
                         date: data[i + 2],
                         message: data[i + 3],
                         author: data[i + 1],
-                        rally: ('DE153949: fix and refactor').match(/US\d{4,}|DE\d{4,}/gi)[0]
+                        rally: (data[i + 3] || '').match(/US\d{4,}|DE\d{4,}/gi)
                     });
                 }
+
+                result.rally = await Promise.all(result.commits.map(commit => commit.rally).unique().filter(rally => !!rally).map(
+                    async ticket => {
+                        let fetchURL = 'https://rally1.rallydev.com/slm/webservice/v2.0/';
+                        if ((ticket + '').startsWith('DE')) {
+                            fetchURL += 'defect';
+                        } else {
+                            fetchURL += 'hierarchicalrequirement';
+                        }
+                        fetchURL += `?query=(FormattedID%20=%20${ticket})`;
+                        fetchURL += '&fetch=true';
+                        const rallyData = await axios.get(
+                            fetchURL,
+                            {
+                                headers: {
+                                    Cookie: "__cfduid=d81cf7e538f21de5c2d4e3849c366ace51564757354; perm-login-locale=en-US; ZSESSIONID=IFvcf6x2RzC5IBd8LT5i5GBTNiNUBnrOkNDsh3858; __cflb=02DiuGt7Pa96k7m2pbYDQ3JqxN9fyiuCTofF21HfPPfPa; JSESSIONID=gc-app-19ey9rgdd7mpdl6vbhtq6yl7he.gc-app-19; SUBBUCKETID=391; SUBSCRIPTIONID=61391; SERVERID=1f05018cd0885e66f7f9cb4067429b0722b672b9; QSI_HistorySession=https%3A%2F%2Frally1.rallydev.com%2F%23%2F290930426388d%2Fdetail%2Fdefect%2F360576610224~1580126980440%7Chttps%3A%2F%2Frally1.rallydev.com%2F%23%2F290930426388d%2Fdetail%2Fuserstory%2F356057887120~1580127077237%7Chttps%3A%2F%2Frally1.rallydev.com%2F%23%2F290930426388d%2Fdashboard~1580136159599%7Chttps%3A%2F%2Frally1.rallydev.com%2F%23%2F290930426388d%2Fdetail%2Fdefect%2F361309643096~1580136261089%7Chttps%3A%2F%2Frally1.rallydev.com%2F%23%2F290930426388d%2Fdetail%2Fuserstory%2F362424128848~1580136381463"}
+                            }
+                        );
+                        try {
+                            return rallyData.data.QueryResult.Results[0];
+                        } catch(error) {
+                            console.log(error)
+                        }
+                    }
+                ));
 
                 resolve({
                     [req.params.name]: result
