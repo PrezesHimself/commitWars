e27bb6667e2559237959432b6b1e65924bf0d3b2
adllcz <aobetkal@gmail.com>
2020-02-19 20:57:47 +0100

    seach bar added

---
+++ front/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,34 @@
+import React, { SetStateAction, Dispatch } from 'react';
+import './SearchBar.css';
+
+import { IonInput, IonLabel, IonItem, IonButton, IonIcon } from '@ionic/react';
+import { playForward } from 'ionicons/icons';
+
+interface SearchProps {
+    searchText: string;
+    setSearchText: any;
+    seachCommiter: any;
+}
+
+const SearchBar: React.FC<SearchProps> = props => {
+    const { setSearchText, searchText, seachCommiter } = props;
+
+    return (
+        <div className="search-container">
+            <IonItem className="search-bar">
+                <IonLabel position="floating">C'mon and search for a Commiter!</IonLabel>
+                <IonInput 
+                    type="text" 
+                    value={searchText} 
+                    onIonChange={event => setSearchText((event.target as HTMLInputElement).value)}>
+                </IonInput>
+            </IonItem>
+            <IonButton onClick={seachCommiter}>
+                <IonIcon icon={playForward} slot="start"></IonIcon>
+                GO!
+            </IonButton>
+        </div>
+    );
+}
+
+export default SearchBar;
