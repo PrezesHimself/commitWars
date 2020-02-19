065b192b03b41b501e20bf5f23dad70e34ef98f3
adllcz <aobetkal@gmail.com>
2020-02-19 22:01:56 +0100

    simple info output

--- front/src/components/InfoOutput/InfoOutput.tsx
+++ front/src/components/InfoOutput/InfoOutput.tsx
@@ -1,18 +1,35 @@
-import React, { Fragment } from 'react';
-import { IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/react';
+import React, { Fragment, useState, useEffect } from 'react';
+import { IonCard, IonCardHeader, IonCardTitle, IonItem } from '@ionic/react';
+
+import * as test from '../../test.json';
 
 const InfoOutput: React.FC = () => {
+    const [data, setData] = useState('');
+
+    useEffect(() => {
+        setData((test as any).default);
+        console.log((test as any).default);
+    }, []);
+
+    const info = data as any;
+
+    useEffect(() => {
+        let releases = info.releases;
+        console.log(releases && Object.keys(releases))
+    })
+
     return (
         <Fragment>
             <IonCard>
                 <IonCardHeader>
-                    <IonCardTitle>Info Output</IonCardTitle>
+                    <IonCardTitle>
+                        {`${info.name}!`}
+                    </IonCardTitle>
                 </IonCardHeader>
-
-                <IonCardContent>
-                    Keep close to Nature's heart... and break clear away, once in awhile,
-                    and climb a mountain or spend a week in the woods. Wash your spirit clean.
-                </IonCardContent>
+                    {
+                        info.releases && Object.keys(info.releases).forEach(release => <IonItem> {release} </IonItem>)
+                    }
+                
             </IonCard>
         </Fragment>
     )
